c4f734ec2afc6973f23572826f285e499576dab5
SEDAT <SEDAT@DESKTOP-1GLHN44>
2022-12-17 20:30:31 +0300

    Proje dosyası ekle.

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,25 @@
+
+Our Data: 22, 27, 16, 2, 18, 6 
+ 1-
+ We choose First(1) smallest member: 2,   27,16,22,18,6
+ Then we choose Second(2) smallest member: 2,6,  16,22,18,27
+ Then we choose Third(3) smallest member: 2,6,16,  22,18,27
+ Then we choose Fourth(4) smallest member: 2,6,16,18,  22,27
+ Then we choose Fifth(5) smallest member:  2,6,16,18,22,  27
+ Then we choose Sixth(6) smallest member:  2,6,16,18,22,27
+ Our Third(3) Fifth(5) and Sixth(6) members are on their same place. Because of that we do not need to change their place.
+
+
+ 2-
+ Big-O Notation= (n)^2
+ 
+ 3- 
+ We use Average Case
+
+ 4-
+ 7,3,5,8,2,9,4,15,6
+
+ 1: 2,3,5,8,7,9,4,15,6
+ 2: 2,3,5,8,7,9,4,15,6
+ 3: 2,3,4,8,7,9,5,15,6
+ 4: 2,3,4,5,7,9,8,15,6
